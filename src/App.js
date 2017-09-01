8f3a9e20bd0be9f803f4ced08a916046ccac8cb2
Chau Hoang <kidphys@gmail.com>
2017-09-01 15:53:50 +0700

    Hook up users control with firebase

--- src/App.js
+++ src/App.js
@@ -2,29 +2,17 @@ import React, { Component } from 'react';
 import { jsonServerRestClient, Admin, Resource, Delete } from 'admin-on-rest';
 
 // see: https://material.io/icons/
-import PostIcon from 'material-ui/svg-icons/action/book';
 import UserIcon from 'material-ui/svg-icons/social/group';
-import DashboardIcon from 'material-ui/svg-icons/social/poll'
 
 /**
 VIEW
 */
-import { PostList, PostEdit, PostCreate } from './posts';
-import { UserList } from './users';
-import { DashboardList, DashboardEdit, DashboardCreate } from './dashboards';
-import { TestList, TestEdit } from './test';
+import { UserList, UserEdit } from './users';
 
 /**
 REST Client
 */
-import myApiRestClient from './restClient';
 import { RestClient, AuthClient } from 'aor-firebase-client';
-import authClient from './authClient';
-
-// import vietnameseMessages from 'aor-language-vietnamese';
-// const messages = {
-// 	'vi': vietnameseMessages,
-// };
 
 const firebaseConfig = {
 	apiKey: 'AIzaSyA_i_1kEu9yJGgReldWXXtEgm3C2Qfm5s4',
@@ -35,15 +23,8 @@ const firebaseConfig = {
 const trackedResources = ['users']
 
 const App = () => (
-    // <Admin restClient={jsonServerRestClient('http://jsonplaceholder.typicode.com')}>
-    // <Admin restClient={myApiRestClient} authClient={authClient} locale="vi" messages={messages}>
-    //   <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} remove={Delete} icon={PostIcon}/>
-    //   <Resource name="users" list={UserList} icon={UserIcon} />
-    //   <Resource name="dashboards" list={DashboardList} edit={DashboardEdit} create={DashboardCreate} icon={DashboardIcon} />
-    // </Admin>
-
-    <Admin restClient={RestClient(trackedResources, firebaseConfig)}>
-      <Resource name="users" list={TestList} edit={TestEdit} icon={PostIcon}/>
+    <Admin restClient={RestClient(trackedResources, firebaseConfig)} authClient={AuthClient}>
+      <Resource name="users" list={UserList} edit={UserEdit} icon={UserIcon}/>
     </Admin>
 );
 
--- src/users.js
+++ src/users.js
@@ -1,14 +1,42 @@
-// in src/users.js
+// in src/posts.js
 import React from 'react';
-import { List, Datagrid, EmailField, TextField } from 'admin-on-rest';
+import { List, Edit, Create, Datagrid, ReferenceField, TextField, 
+	EditButton, DisabledInput, LongTextInput, ReferenceInput, 
+	SelectInput, SimpleForm, TextInput,
+	Filter,
+    BooleanField, BooleanInput
+} from 'admin-on-rest';
+	
+const UserFilter = (props) => (
+    <Filter {...props}>
+        <TextInput label="Search" source="q" alwaysOn />
+    </Filter>
+);
 
 export const UserList = (props) => (
-    <List title="All users" {...props}>
+    <List {...props} filters={<UserFilter/>}>
         <Datagrid>
             <TextField source="id" />
-            <TextField source="name" />
-            <TextField source="username" />
-            <EmailField source="email" />
+            <BooleanField source="account.active" />
+            <TextField source="profile.email" />
+            <TextField source="account.expiredDate" />
+            <EditButton />
         </Datagrid>
     </List>
 );
+
+const UserTitle = ({ record }) => {
+    return <span>Editing: {record ? `"${record.name}"` : ''}</span>;
+};
+
+export const UserEdit = (props) => (
+    <Edit title={<UserTitle />} {...props}>
+        <SimpleForm>
+            <DisabledInput source="id" />
+            <BooleanInput label="Active?" source="account.active" />
+            <TextInput source="profile.email" />
+            <TextInput source="account.expiredDate" />
+        </SimpleForm>
+    </Edit>
+);
+
