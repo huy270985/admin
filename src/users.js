f4a104a2a4cb030c141af0412cb4abffaa6892ad
Chau Hoang <kidphys@gmail.com>
2017-09-04 19:40:48 +0700

    Add active user filter

--- src/users.js
+++ src/users.js
@@ -14,6 +14,7 @@ import { List, Edit, Create, Datagrid, ReferenceField, TextField,
 const UserFilter = (props) => (
     <Filter {...props}>
         <TextInput label="Search" source="q" alwaysOn />
+        <BooleanInput label="Active?" source="account.active" alwaysOn />
     </Filter>
 );
 
